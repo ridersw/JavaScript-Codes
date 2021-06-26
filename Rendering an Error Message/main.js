cd114ccd14ef20b3e94246cce29b24fb08b811cf
Shashiraj Walsetwar <wshashiraj@gmail.com>
2021-06-26 20:00:15 +0530

    How to Render Simple Error Message

---
+++ Rendering an Error Message/main.js
@@ -0,0 +1,6 @@
+//something went wrong, please try again
+
+function displayError() {
+	//document.getElementById("error").innerText = "Something Went Wrong, Please Try Again!!!"
+	document.getElementById("error").textContent = "Something Went Wrong, Please Try Again!!!"	
+} 
